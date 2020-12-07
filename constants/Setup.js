8288793f88b6da93bc0012e004f4de8e32bc86e9
tabbykatz <tomelay@gmail.com>
2020-12-06 19:40:09 -0800

    new setup

--- constants/Setup.js
+++ constants/Setup.js
@@ -14,6 +14,8 @@ export const SETUP = [
       { name: 'MacBook Pro 16″', description: '2.3 GHz 8-Core Intel Core i9', link: 'https://www.apple.com/macbook-pro-16/' },
       { name: 'Apple Magic Trackpad 2', description: 'I try to avoid mouse-like behaviour when possible.', link: 'https://www.apple.com/shop/product/MJ2R2LL/A/magic-trackpad-2-silver' },
       { name: 'Dell 24 Monitor - P2419H', description: 'External monitor joy, one vertically and one horizontally.', link: 'https://www.dell.com/en-us/work/shop/dell-24-monitor-p2419h/apd/210-aqdx/monitors-monitor-accessories' },
+      { name: 'TaoTronics Bluetooth 5.0', description: 'A good bluetooth soundbar for my classes.', link: 'https://www.amazon.com/gp/product/B07SLY8D7C/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1&fpw=alm' },
+      { name: 'TONOR Condenser Mic', description: 'A new mic for those all-day Zoom Fests when the ears beg for a break from the headset!', link: 'https://www.amazon.com/gp/product/B08CVP2HXP/ref=ppx_yo_dt_b_search_asin_title?ie=UTF8&psc=1&fpw=alm'},
       { name: 'Beats Solo Pro', description: 'Trying these out for video conferencing, so far so good. Killer noise cancelling!', link: 'https://www.beatsbydre.com/headphones/solo-pro' },
 	  { name: 'Varmilo Sakura Pink', description: 'Multi Dye Sub PBT with Pink legends, Cherry brown switches.', link: 'https://mechanicalkeyboards.com/shop/index.php?l=product_detail&p=3508'}
     ]
