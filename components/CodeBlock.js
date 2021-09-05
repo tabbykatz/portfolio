aedef2058c011485023896b90eab29ac8caa2952
tabbykatz <tomelay@gmail.com>
2021-09-04 19:43:15 -0700

    update color theme

--- components/CodeBlock.js
+++ components/CodeBlock.js
@@ -1,24 +1,24 @@
-import React, { PureComponent } from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { anOldHope } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
+import React, { PureComponent } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { nord } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const preStyle = {
   borderRadius: 6,
-  padding: '1.7em',
-  lineHeight: '2.3em',
-}
+  padding: "1.7em",
+  lineHeight: "2.3em",
+};
 
 const codeProps = {
   style: {
     fontFamily: `'Space Mono', 'ibm-plex-mono'`,
-    fontSize: '18.5px'
-  }
-}
+    fontSize: "18.5px",
+  },
+};
 
 class CodeBlock extends PureComponent {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       removeLines: [],
@@ -26,75 +26,96 @@ class CodeBlock extends PureComponent {
       updateLines: [],
       copiedToClipboard: false,
       displayCopyButton: false,
-    }
+    };
   }
 
   copyToClipboard = () => {
-    this.setState({
-      ...this.state,
-      copiedToClipboard: true
-    }, () => {
-      setTimeout(() => {
-        this.setState({
-          ...this.state,
-          copiedToClipboard: false
-        })
-      }, 3500)
-    })
-  }
+    this.setState(
+      {
+        ...this.state,
+        copiedToClipboard: true,
+      },
+      () => {
+        setTimeout(() => {
+          this.setState({
+            ...this.state,
+            copiedToClipboard: false,
+          });
+        }, 3500);
+      }
+    );
+  };
 
   componentDidMount() {
     const { language } = this.props;
-    const linesObj = language && language.split(":")[1]
+    const linesObj = language && language.split(":")[1];
 
     if (linesObj) {
-      const splittedValues = linesObj.split(',')
-      let stateLabel
+      const splittedValues = linesObj.split(",");
+      let stateLabel;
       let linesToUpdate = {
-        removeLines: [], addLines: [], updateLines: [],
-      }
+        removeLines: [],
+        addLines: [],
+        updateLines: [],
+      };
 
-      splittedValues.map(lines => {
-        const linesRange = lines.split(',')
+      splittedValues.map((lines) => {
+        const linesRange = lines.split(",");
 
-        linesRange.map(eachLine => {
-          const splitted = eachLine.split('-')
+        linesRange.map((eachLine) => {
+          const splitted = eachLine.split("-");
 
-          if (splitted[0] === '') { // Is removing lines
-            splitted.shift()
-            stateLabel = 'removeLines'
-          } else if (splitted[0] === '!') {
-            splitted.shift()
-            stateLabel = 'updateLines'
+          if (splitted[0] === "") {
+            // Is removing lines
+            splitted.shift();
+            stateLabel = "removeLines";
+          } else if (splitted[0] === "!") {
+            splitted.shift();
+            stateLabel = "updateLines";
           } else {
-            stateLabel = 'addLines'
+            stateLabel = "addLines";
           }
 
           if (splitted.length > 1) {
-            for (let i = parseInt(splitted[0]); i <= parseInt(splitted[1]); i++) {
-              linesToUpdate[stateLabel].push(i)
+            for (
+              let i = parseInt(splitted[0]);
+              i <= parseInt(splitted[1]);
+              i++
+            ) {
+              linesToUpdate[stateLabel].push(i);
             }
-          } else { // Only one liner
-            linesToUpdate[stateLabel].push(parseInt(splitted[0]))
+          } else {
+            // Only one liner
+            linesToUpdate[stateLabel].push(parseInt(splitted[0]));
           }
 
           this.setState({
             [stateLabel]: [
-              ...this.state[stateLabel], ...linesToUpdate[stateLabel]
-            ]
-          })
-        })
-      })
+              ...this.state[stateLabel],
+              ...linesToUpdate[stateLabel],
+            ],
+          });
+        });
+      });
     }
   }
 
   toggleCopyButton = () => {
-    this.setState({ ...this.state, displayCopyButton: !this.state.displayCopyButton })
-  }
+    this.setState({
+      ...this.state,
+      displayCopyButton: !this.state.displayCopyButton,
+    });
+  };
 
   render() {
     const { language, value } = this.props;
-    const { addLines, removeLines, updateLines, copiedToClipboard, displayCopyButton } = this.state
+    const {
+      addLines,
+      removeLines,
+      updateLines,
+      copiedToClipboard,
+      displayCopyButton,
+    } = this.state;
 
     return (
       <div
@@ -103,40 +124,42 @@ class CodeBlock extends PureComponent {
       >
         <SyntaxHighlighter
           language={language}
-          style={anOldHope}
+          style={nord}
           customStyle={preStyle}
           codeTagProps={codeProps}
           wrapLines
-          lineProps={lineNumber => {
-            const mergedLines = addLines.concat(removeLines).concat(updateLines)
-            let style = { display: 'block' };
+          lineProps={(lineNumber) => {
+            const mergedLines = addLines
+              .concat(removeLines)
+              .concat(updateLines);
+            let style = { display: "block" };
 
             if (mergedLines.includes(lineNumber)) {
               style = {
                 ...style,
-                margin: '0 -22px',
-                padding: '3px 12px 6px'
-              }
+                margin: "0 -22px",
+                padding: "3px 12px 6px",
+              };
             }
 
             if (removeLines.includes(lineNumber)) {
               style = {
                 ...style,
                 borderLeft: `6px #f9320c solid`,
-                background: `rgba(249, 50, 12, .1)`
-              }
+                background: `rgba(249, 50, 12, .1)`,
+              };
             } else if (addLines.includes(lineNumber)) {
               style = {
                 ...style,
                 borderLeft: `6px #3ac569 solid`,
-                background: `rgba(58, 197, 105, .1)`
-              }
+                background: `rgba(58, 197, 105, .1)`,
+              };
             } else if (updateLines.includes(lineNumber)) {
               style = {
                 ...style,
                 borderLeft: `6px #f9c00c solid`,
-                background: `rgba(249, 192, 12, .1)`
-              }
+                background: `rgba(249, 192, 12, .1)`,
+              };
             }
 
             return { style };
@@ -147,9 +170,7 @@ class CodeBlock extends PureComponent {
 
         <div className="copy-to-clipboard">
           <CopyToClipboard text={value} onCopy={() => this.copyToClipboard()}>
-            <button>
-              {copiedToClipboard ? 'Copied ✔' : 'Copy'}
-            </button>
+            <button>{copiedToClipboard ? "Copied ✔" : "Copy"}</button>
           </CopyToClipboard>
         </div>
       </div>
@@ -157,4 +178,4 @@ class CodeBlock extends PureComponent {
   }
 }
 
-export default CodeBlock
+export default CodeBlock;
