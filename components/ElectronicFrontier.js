78eadb6a63c6c11059344aa9dc26a30a1c2d9d84
Tabitha O'Melay <tomelay@gmail.com>
2021-11-20 18:57:03 -0800

    fix mess

--- components/ElectronicFrontier.js
+++ components/ElectronicFrontier.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import * as React from "react";
 
 const ACTION_LINK = `https://act.eff.org/`;
 
-function ElectronicFrontier() {
-  return (
-    <div className="eff">
-      <span>
-        I Fight Surveillance. {' '}
+const ElectronicFrontier = () => (
+  <div className="eff">
+    <span>
+      I Fight Surveillance.{" "}
+      <a href={ACTION_LINK} target="_blank" rel="noopener noreferrer nofollow">
+        Join EFF
+      </a>
+      .
+    </span>
+  </div>
+);
 
-        <a href={ACTION_LINK} target="_blank" rel="noopener noreferrer nofollow">
-		Join EFF
-	  </a>.
-      </span>
-    </div>
-  )
-}
-
-export default ElectronicFrontier
+export default ElectronicFrontier;
